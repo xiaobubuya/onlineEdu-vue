31266cc4862f56cf138ea8d912985d4a4c0bb319
424102378@qq.com <424102378@qq.com>
2021-06-28 16:44:28 +0800

    6-28 网关

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:9001"',
+  BASE_API: '"http://localhost:8222"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
   OSS_PATH: '"https://xiaobubuya-edu.oss-cn-beijing.aliyuncs.com"'
 })
--- src/views/statistics/daily/chart.vue
+++ src/views/statistics/daily/chart.vue
@@ -102,6 +102,39 @@ export default {
 
       // 指定图表的配置项和数据
       var option = {
+        title: {
+          text: '数据统计'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        dataZoom: [{
+          show: true,
+          height: 30,
+          xAxisIndex: [
+            0
+          ],
+          bottom: 30,
+          start: 10,
+          end: 80,
+          handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
+          handleSize: '110%',
+          handleStyle: {
+            color: '#d3dee5'
+
+          },
+          textStyle: {
+            color: '#fff'
+          },
+          borderColor: '#90979c'
+        },
+          {
+            type: 'inside',
+            show: true,
+            height: 15,
+            start: 1,
+            end: 35
+          }],
         xAxis: {
           type: 'category',
           data: this.xData//-------绑定数据
